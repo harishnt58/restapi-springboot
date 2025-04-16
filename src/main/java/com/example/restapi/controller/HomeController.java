cf1f7c08f0a25893faf00eba3d7aa3356d480e4e
harishnt-58 <4jn18cs031cse@gmail.com>
2025-04-16 16:03:47 +0530

    First commit

---
+++ src/main/java/com/example/restapi/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.restapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api/")
+public class HomeController {
+    @GetMapping("home/{name}")
+    public String home(@PathVariable String name){
+        return "Hello "+name;
+    }
+}
